a16695ff2754cfb47d711fe7cbfed26825471f23
Alex G <alex.ghiurau@gmail.com>
2023-12-05 18:58:20 +0200

    add demo `data`

---
+++ src/data/data.js
@@ -0,0 +1,383 @@
+export default [
+    {
+        "id":42,
+        "title":"Scoala Constantin Brâncuși",
+        "description":"Mazerschi",
+        "address":"Cluj-Napoca, România",
+        "lat":46.7604365,
+        "lng":23.6165151,
+        "pictures":["42_0.jpg"]
+    },
+    {
+        "id":41,
+        "title":"Emozia roastery",
+        "description":"Emozia",
+        "address":"Cluj-Napoca, România",
+        "lat":46.7672512,
+        "lng":23.6023848,
+        "pictures":["41_0.jpg"]
+    },
+    {
+        "id":40,
+        "title":"Baza sportivă La Terenuri Mănăștur",
+        "description":"Mazerschi",
+        "address":"Cluj-Napoca, România",
+        "lat":46.7490748,
+        "lng":23.5545953,
+        "pictures":["40_0.jpg", "40_1.jpg"]
+    },
+    {
+        "id":39,
+        "title":"Pediatrie II",
+        "description":"Kero și Ocu pentru Asociația Magic",
+        "address":"3-5, Crișan, Cluj-Napoca, România",
+        "lat":46.7782483,
+        "lng":23.5860596,
+        "pictures":["39_0.jpg", "39_1.jpg", "39_2.jpg", "39_3.jpg"]
+    },
+    {
+        "id":38,
+        "title":"Euphoria Biergarten",
+        "description":"ArtMikio",
+        "address":"2, Cardinal Iuliu Hosu, Cluj-Napoca, România",
+        "lat":46.7691748,
+        "lng":23.5818895,
+        "pictures":["38_0.jpg", "38_1.jpg"]
+    },
+    {
+        "id":37,
+        "title":"The blooming bird",
+        "description":"PAO. The blooming bird.",
+        "address":"79, Decebal, Cluj-Napoca, România",
+        "lat":46.7825475,
+        "lng":23.5905320,
+        "pictures":["37_0.jpg", "37_1.jpg", "37_2.jpg"]
+    },
+    {
+        "id":29,
+        "title":"Liceul de arte vizuale Romulus Ladea",
+        "description":"Andrei Tonița, Kero zen Ocu și Irlo, Flaviu Moldovan",
+        "address":"Cluj-Napoca, România",
+        "lat":46.772757,
+        "lng":23.605906,
+        "pictures":["29_0.jpg", "29_1.jpg", "29_2.jpg", "29_3.jpg", "29_4.jpg", "29_5.jpg"]
+    },
+    {
+        "id":28,
+        "title":"Cristian Scutaru",
+        "description":"Bosch",
+        "address":"Cluj-Napoca, România",
+        "lat":46.777392,
+        "lng":23.595097,
+        "pictures":["28_0.jpg", "28_1.jpg", "28_2.jpg"]
+    },
+    {
+        "id":31,
+        "title":"Kero Zen, Ocu, Studio Karta",
+        "description":"Strada Constanța",
+        "address":"Cluj-Napoca, România",
+        "lat":46.773369,
+        "lng":23.596221,
+        "pictures":["31_0.jpg", "31_1.jpg", "31_2.jpg", "31_3.jpg", "31_4.jpg", "31_5.jpg", "31_6.jpg"]
+    },
+    {
+        "id":27,
+        "title":"Kero și Ocu, Brothers of Light",
+        "description":"Căminul subcultural",
+        "address":"Cluj-Napoca, România",
+        "lat":46.782503,
+        "lng":23.585968,
+        "pictures":["27_0.jpg", "27_1.jpg", "27_2.jpg"]
+    },
+    {
+        "id":33,
+        "title":"OBERT",
+        "description":"OBERT",
+        "address":"Cluj-Napoca, România",
+        "lat":46.772474,
+        "lng":23.604103,
+        "pictures":["33_0.jpg", "33_1.jpg"]
+    },
+    {
+        "id":32,
+        "title":"Kero Zen",
+        "description":"Strada Traian",
+        "address":"Cluj-Napoca, România",
+        "lat":46.778037,
+        "lng":23.591416,
+        "pictures":["32_0.jpg"]
+    },
+    {
+        "id":36,
+        "title":"Novo ordem moralis. Kero Zen, Ocu, Jester, Nov",
+        "description":"Șapca lui Ceaușescu",
+        "address":"Cluj-Napoca, România",
+        "lat":46.780983,
+        "lng":23.625682,
+        "pictures":["36_0.jpg", "36_1.jpg", "36_2.jpg", "36_3.jpg"]
+    },
+    {
+        "id":1,
+        "title":"Pod Elisabeta",
+        "description":"Pod Elisabeta. Metal pe malul stâng și stencil + paste up pe cel drept",
+        "address":"28, Emil Isac, Cluj-Napoca, România",
+        "lat":46.7724605,
+        "lng":23.584037499999997,
+        "pictures":["1_0.jpg", "1_1.jpg", "1_2.jpg"]
+    },
+    {
+        "id":2,
+        "title":"Samsara",
+        "description":"Samsara. Obie platon",
+        "address":"3, Iuliu Hosu, Cluj-Napoca, România",
+        "lat":46.770763099999996,
+        "lng":23.5836468,
+        "pictures":["2_0.jpg", "2_1.jpg", "2_2.jpg", "2_3.jpg"]
+    },
+    {
+        "id":3,
+        "title":"Shto collage bar",
+        "description":"La ieșire din gang, în curte",
+        "address":"6, Memorandumului, Cluj-Napoca, România",
+        "lat":46.770136199999996,
+        "lng":23.5876126,
+        "pictures":["3_0.jpg", "3_1.jpg", "3_2.jpg", "3_3.jpg", "3_4.jpg", "3_5.jpg"]
+    },
+    {
+        "id":4,
+        "title":"Insomnia",
+        "description":"Insomnia. Curtea de jos. Kero Zen și Ocu",
+        "address":"2 Universității, Cluj-Napoca, România",
+        "lat":46.7682049,
+        "lng":23.5903335,
+        "pictures":["4_0.jpg", "4_1.jpg", "4_2.jpg", "4_3.jpg", "4_4.jpg", "4_5.jpg", "4_6.jpg", "4_7.jpg", "4_8.jpg", "4_9.jpg", "4_10.jpg", "4_11.jpg", "4_12.jpg", "4_13.jpg", "4_14.jpg"]
+    },
+    {
+        "id":5,
+        "title":"Victoria",
+        "description":"Pe zidurile clădirilor din centrul vechi pot fi descoperite stencilsuri noi și vechi",
+        "address":"Centrul vechi, Cluj-Napoca, România",
+        "lat":46.767361,
+        "lng":23.588712,
+        "pictures":["5_0.jpg", "5_1.jpg", "5_2.jpg", "5_3.jpg", "5_4.jpg", "5_5.jpg", "5_6.jpg", "5_7.jpg"]
+    },
+    {
+        "id":6,
+        "title":"Pasaj Racoviță",
+        "description":"Pasaj. Probabil primul graffiti adevărat din Cluj. Kero",
+        "address":"12, Mihail Kogălniceanu, Cluj-Napoca",
+        "lat":46.767782399999994,
+        "lng":23.5940355,
+        "pictures":["6_0.jpg", "6_1.jpg", "6_2.jpg", "6_3.jpg", "6_4.jpg", "6_5.jpg", "6_6.jpg"]
+    },
+    {
+        "id":7,
+        "title":"Liceul Coșbuc",
+        "description":"In curtea interioara a Colegiului Național George Coșbuc. Cătălina Milea. Susținut de NTT Data",
+        "address":"70, Avram Iancu, Cluj-Napoca, România",
+        "lat":46.7673229,
+        "lng":23.5963761,
+        "thumbnail":"",
+        "pictures":["7_0.jpg"]
+    },
+    {
+        "id":8,
+        "title":"Kero, Dorobanților",
+        "description":"Kero și Obie platon",
+        "address":"72, Calea Dorobanților, Cluj-Napoca, România",
+        "lat":46.773415799999995,
+        "lng":23.607482599999997,
+        "pictures":["8_0.jpg", "8_1.jpg"]
+    },
+    {
+        "id":9,
+        "title":"Panda",
+        "description":"Dorobanților Residence. Panda",
+        "address":"132-134, Calea Dorobanților, Cluj-Napoca, România",
+        "lat":46.775488200000005,
+        "lng":23.6131863,
+        "pictures":["9_0.jpg"]
+    },
+    {
+        "id":10,
+        "title":"Timaf",
+        "description":"Pod Mărăști. Timaf urban art",
+        "address":"10, Aurel Vlaicu, Cluj-Napoca, România",
+        "lat":46.778827,
+        "lng":23.618748000000004,
+        "pictures":["10_0.jpg", "10_1.jpg", "10_2.jpg", "10_3.jpg", "10_4.jpg", "10_5.jpg", "10_6.jpg", "10_7.jpg", "10_8.jpg", "10_9.jpg", "10_10.jpg", "10_11.jpg", "10_12.jpg", "10_13.jpg", "10_14.jpg", "10_15.jpg", "10_16.jpg", "10_17.jpg", "10_18.jpg", "10_19.jpg", "10_20.jpg", "10_21.jpg", "10_22.jpg"]
+    },
+    {
+        "id":11,
+        "title":"Kero, Dunării",
+        "description":"Kero",
+        "address":"16, Teleorman, Cluj-Napoca, România",
+        "lat":46.781181499999995,
+        "lng":23.619534899999998,
+        "pictures":["11_0.jpg", "11_1.jpg", "11_2.jpg"]
+    },
+    {
+        "id":12,
+        "title": "Vhils",
+        "description":"La intrarea în curtea Fabricii de pensule, respectiv la intrarea în clădirea principală",
+        "address": "59-61, Henri Barbusse, Cluj-Napoca, România",
+        "lat":46.7842808,
+        "lng":23.608603499999997,
+        "pictures": ["12_0.jpg", "12_1.jpg"]
+    },
+    {
+        "id":13,
+        "title":"Business center",
+        "description":"Lângă Cluj Bussines Center",
+        "address":"46, Henri Barbusse, Cluj-Napoca, România",
+        "lat":46.782907599999994,
+        "lng":23.6078811,
+        "pictures":["13_0.jpg", "13_1.jpg", "13_2.jpg", "13_3.jpg", "13_4.jpg", "13_5.jpg", "13_6.jpg"]
+    },
+    {
+        "id":14,
+        "title":"Gara",
+        "description":"Vagoane dezafectate în gara din Cluj-Napoca. A se vedea ambele parți",
+        "address":"1-3, Piața Gării, Cluj-Napoca, România",
+        "lat":46.784355,
+        "lng":23.586143999999997,
+        "pictures":["14_0.jpg", "14_1.jpg", "14_2.jpg", "14_3.jpg", "14_4.jpg", "14_5.jpg", "14_6.jpg", "14_7.jpg", "14_8.jpg", "14_9.jpg", "14_10.jpg", "14_11.jpg", "14_12.jpg"]
+    },
+    {
+        "id":15,
+        "title":"Micro macro",
+        "description":"Kero",
+        "address":"33, Horea, Cluj-Napoca, România",
+        "lat":46.778737400000004,
+        "lng":23.586997399999998,
+        "thumbnail":"",
+        "pictures":["15_0.jpg", "15_1.jpg", "15_2.jpg", "15_3.jpg", "15_4.jpg", "15_5.jpg"]
+    },
+    {
+        "id":16,
+        "title":"Samsara veche",
+        "description":"Samsara. Kero",
+        "address":"5, Ludwig Roth, Cluj-Napoca, România",
+        "lat":46.775036799999995,
+        "lng":23.586662099999998,
+        "pictures":["16_0.jpg", "16_1.jpg"]
+    },
+    {
+        "id":17,
+        "title":"Ștrand municipal",
+        "description":"Piscina dezafectată. Graffiti în vechiul ștrand municipal.",
+        "address":"Aleea Stadionului, Cluj-Napoca, România",
+        "lat":46.7670908,
+        "lng":23.5720572,
+        "pictures":["17_0.jpg", "17_1.jpg", "17_2.jpg", "17_3.jpg"]
+    },
+    {
+        "id":18,
+        "title":"Cotton",
+        "description":"Cotton Cluj. Pe malul Someșului Mic",
+        "address":"35-37, Bulevardul 1 Decembrie, Cluj-Napoca, România",
+        "lat":46.768113,
+        "lng":23.565793,
+        "pictures":["18_0.jpg"]
+    },
+    {
+        "id":19,
+        "title":"Ghibu",
+        "description":"Liceul Onisifor Ghibu. Kero – pe clădirea principală. Alte graffitiuri pe spatele sălii de sport",
+        "address":"12, Alexandru Vlahuta, Cluj-Napoca, România",
+        "lat":46.770573999999996,
+        "lng":23.563979999999997,
+        "thumbnail":"",
+        "pictures":["19_0.jpg", "19_1.jpg", "19_2.jpg", "19_3.jpg", "19_4.jpg", "19_5.jpg", "19_6.jpg"]
+    },
+    {
+        "id": 20,
+        "title": "Ocu",
+        "description": "Ocu",
+        "address":"Plaja Grigorescu, Cluj-Napoca, România",
+        "lat":46.765851,
+        "lng":23.555008,
+        "pictures":["20_0.jpg"]
+    },
+    {
+        "id":21,
+        "title":"Școala Goga",
+        "description":"Scoala Octavian Goga. Kero, Ocu, Waone, Lost Optics, Irlo",
+        "address":"16, Aleea Peana, Cluj-Napoca, România",
+        "lat":46.754661,
+        "lng":23.549865999999998,
+        "pictures":["21_0.jpg", "21_1.jpg", "21_2.jpg", "21_3.jpg", "21_4.jpg", "21_5.jpg", "21_6.jpg", "21_7.jpg", "21_8.jpg", "21_9.jpg", "21_10.jpg", "21_11.jpg", "21_12.jpg", "21_13.jpg", "21_14.jpg", "21_15.jpg", "21_16.jpg", "21_17.jpg"]
+    },
+    {
+        "id": 22,
+        "title": "Cinema Dacia",
+        "description": "Cinema Dacia. Kero coordonând un grup de elevi",
+        "address":"11, Bucegi, Cluj-Napoca, România",
+        "lat":46.754704499999995,
+        "lng":23.557318499999997,
+        "pictures":["22_0.jpg", "22_1.jpg", "22_2.jpg"]
+    },
+    {
+        "id":23,
+        "title":"Garaj 1",
+        "description":"Garaj. Petre",
+        "address":"38-40, Mehedinți, Cluj-Napoca, România",
+        "lat":46.7532048,
+        "lng":23.556724499999998,
+        "pictures":["23_0.jpg"]
+    },
+    {
+        "id":24,
+        "title":"Garaj 2",
+        "description":"Garaj langă podul peste pârâu",
+        "address":"50-52, Mehedinți, Cluj-Napoca, România",
+        "lat":46.752105,
+        "lng":23.555848899999997,
+        "pictures":["24_0.jpg"]
+    },
+    {
+        "id":25,
+        "title":"Tram",
+        "description":"In Cluj-Napoca circulă trei tramvaie decorate de artiști stradali: Ocu, Kero, Irlo",
+        "address":"Cluj-Napoca, România",
+        "lat":46.773685,
+        "lng":23.588017,
+        "pictures":["25_0.jpg", "25_1.jpg"]
+    },
+    {
+        "id":26,
+        "title":"Jester",
+        "description":"Cafenea Papillon",
+        "address":"Cluj-Napoca, România",
+        "lat":46.769868,
+        "lng":23.584916,
+        "pictures":["26_0.jpg"]
+    },
+    {
+        "id":30,
+        "title":"Cluj Business Campus (CBC) rooftop",
+        "description":"CBC rooftop",
+        "address":"Cluj-Napoca, România",
+        "lat":46.783018,
+        "lng":23.608002,
+        "pictures":["30_0.jpg", "30_1.jpg"]
+    },
+    {
+        "id":34,
+        "title":"Gang Piața Cipariu",
+        "description":"Gang Piața Cipariu",
+        "address":"Cluj-Napoca, România",
+        "lat":46.768179,
+        "lng":23.599723,
+        "pictures":["34_0.jpg", "34_1.jpg", "34_2.jpg"]
+    },
+    {
+        "id":35,
+        "title":"Garlic",
+        "description":"Restaurat Garlic",
+        "address":"Cluj-Napoca, România",
+        "lat":46.767373,
+        "lng":23.588449,
+        "pictures":["35_0.jpg", "35_1.jpg", "35_2.jpg"]
+    }
+]
